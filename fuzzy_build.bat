65d2fcb9f5c8f793cc2285c0aa7a7b68808a1ae7
wiranoid <wiranoid@gmail.com>
2017-11-12 14:57:50 +0300

    .sln config

--- fuzzy_build.bat
+++ fuzzy_build.bat
@@ -5,5 +5,5 @@ set libs_dir=%build_dir%\libs
 
 if not exist %build_dir% mkdir %build_dir%
 pushd %build_dir%
-cl -MT -I ..\externals\glfw\include -Zi ..\src\fuzzy.cpp ..\%libs_dir%\glfw3.lib opengl32.lib shell32.lib gdi32.lib user32.lib
+cl -MTd -I ..\externals\glfw\include -Zi ..\src\fuzzy.cpp ..\%libs_dir%\glfw3.lib opengl32.lib shell32.lib gdi32.lib user32.lib
 popd
